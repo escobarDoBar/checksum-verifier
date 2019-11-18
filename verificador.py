a11f8442ef31e567bbc92a6797ce6179f19403cf
WrenchEscobar <ju_pn2010@hotmail.com>
2019-11-18 11:38:50 -0300

    Provável  finalização do projeto

--- verificador.py
+++ verificador.py
@@ -1,63 +1,85 @@
 #!/usr/bin/env python
 import sqlite3
 import subprocess
+import sys
+import getopt
+import csv
 
 def acharBanco():
-    print("Aqui terá a função que encontrará o caminho do banco de dados SQLite do usuário")
+    return banco = "~/banco.sqlite"
 
 
 def pesquisarBanco(pesquisa):
     conn = sqlite3.connect(acharBanco())
     c = conn.cursor()
-    link = c.execute("SELECT linkDownload FROM tabela_q_esqueci_o_nome WHERE variável = ?", pesquisa) # Tabela, coluna e variavel possuem esses nomes porque esqueci seus nomes reais :)
+    link = c.execute("SELECT linkDownload FROM cacheDownload WHERE arquivo = ?", pesquisa) # Tabela, coluna e variavel possuem esses nomes porque esqueci seus nomes reais :)
+    return link
 
-def baixarCheck(link):
-    # Realizar modificação no link para retornar pelo menos 1 diretório para realizar a pesquisa dos arquivos disponíveis
-    # link = {código que vai fazer isso}
-    links = subprocess.call(["goofile ", "-d ", link, " -f ", "txt ", "| ", "grep ", "txt"]) # Realizar mais de uma pesquisa caso necessário
+def baixarCheck(pesquisa):
+    http,link,resto = pesquisa.slipt("/", 3)
+    linkPesquisa = http+"//"+link
+    
+    links = subprocess.run(["goofile", "-d", linkPesquisa, "-f", "gpg", "|", "grep", "gpg", ">", "/tmp/links.txt"]) # Realizar mais de uma pesquisa caso necessário
+    
+    f = open('/tmp/links.txt', 'r')
+    links = f.readlines()
+    f.close()
     # Serão quebrados os links encontrados para o usuário conseguir escolher de quais deles deseja baixar, caso haja apenas um link o usuário não terá a opção de escolha
+    # Falta fazer a linha de cima
 
     # Loop feito que realizará a exibição do links
     y = 1
     for x in links:
-        print(y, ") ", x)
+        print(y+") "+x)
         y += 1
 
-    # Tenho que descobrir qual tipo de Python usarei e então escolher qual dos dois inputs vou usar
-    opcao = raw_input('Enter your input:')  # If you use Python 2
-    opcao = input('Enter your input:')      # If you use Python 3
+    opcao = input('Coloque o número realacionado ao link que desejas baixar: ')
 
     # Salva o link conforme a escolha do usuário
     y = 1
     for x in links:
         if y == opcao:
-            link = x
+            linkGPG = x
         y += 1
 
+    
+
     # Baixa o arquivo para realziar a verificação
-    subprocess.call(["wget", "-f", link])
+    sucessoGPG = subprocess.run(["wget", "-o", "verificar.gpg", "-f", link])
+    if sucessoGPG == 0:    
+        print("Arquivo GPG baixado com sucesso")
+    else:
+        print("Houve um erro ao baixar o arquivo GPG")
 
-# Talvez vou usar essa função
-def md5Arquivo(arquivo):
-    hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
+    linkSHA = linkGPG.slipt(".gpg")
+    sucessoSHA = subprocess.run(["wget", "-o", "sha256sum.txt", "-f", linkSHA])
+    if sucessoSHA == 0:    
+        print("Arquivo SHA baixado com sucesso")
+    else:
+        print("Houve um erro ao baixar o arquivo SHA")
 
-# Criar função que gerará o md5 ou seja lá qual for o negócio e comparar com o baixado pela função baixarCheck()
 def verificarCheck(checksum):
-    subprocess.call(["sha256sum", "-c", checksum])
-    # Printar o resultado da verificação
+    print(subprocess.run([(["sha256sum", "-c", checksum], capture_output=True)
 
 # Função principal que rodará o programa
 def main():
 
+    try:
+      opts, args = getopt.getopt(argv,"hc:",["check="])
+    except getopt.GetoptError as err:
+      print(err)  
+      print('Forma de usar: verificador.py -c arquivo_baixado')
+      sys.exit(2)
+    for opt, arg in opts:
+      if opt == '-h':
+         print('verificador.py -c arquivo_baixado')
+         sys.exit()
+      elif opt in ("-c", "--check"):
+         arquivo = arg
+
     # Descobre o link do checksum e baixa ele
-    link = pesquisarBanco($0)
+    link = pesquisarBanco(arquivo)
     baixarCheck(link)
 
     # Verifica o checksum
-    verificarCheck(variavelOpção)
-    
-
+    verificarCheck("sha256sum.txt")
