ead8829a0cd45010e405e1a3113767114f93a28c
WrenchEscobar <ju_pn2010@hotmail.com>
2019-11-20 07:43:37 -0300

    O bug do DB foi resolvido

--- verificador.py
+++ verificador.py
@@ -12,45 +12,40 @@ def acharBanco():
 def pesquisarBanco(pesquisa):
     conn = sqlite3.connect(acharBanco())
     c = conn.cursor()
-    link = c.execute("SELECT linkDownload FROM cacheDownload WHERE arquivo = ?", pesquisa) # Tabela, coluna e variavel possuem esses nomes porque esqueci seus nomes reais :)
+    link = c.execute("SELECT linkDownload FROM cacheDownload WHERE arquivo = '? ?", (pesquisa, "'")) # Tabela, coluna e variavel possuem esses nomes porque esqueci seus nomes reais :)
     return link
 
 def baixarCheck(pesquisa):
-    http,link,resto = pesquisa.slipt("/", 3)
-    linkPesquisa = http+"//"+link
-    
+    link = pesquisa.slipt("/", 3)
+    linkPesquisa = link[0]+"//"+link[1]
     links = subprocess.run(["goofile", "-d", linkPesquisa, "-f", "gpg", "|", "grep", "gpg", ">", "/tmp/links.txt"]) # Realizar mais de uma pesquisa caso necessário
-    
     f = open('/tmp/links.txt', 'r')
     links = f.readlines()
     f.close()
     # Serão quebrados os links encontrados para o usuário conseguir escolher de quais deles deseja baixar, caso haja apenas um link o usuário não terá a opção de escolha
     # Falta fazer a linha de cima
-
     # Loop feito que realizará a exibição do links
     y = 1
-    for x in links:
-        print(y+") "+x)
+    x = 6 # ou 5, não lembro
+    while x < len(links):
+        print(y+") "+links[x])
         y += 1
-
+        x += 1
     opcao = input('Coloque o número realacionado ao link que desejas baixar: ')
-
     # Salva o link conforme a escolha do usuário
     y = 1
-    for x in links:
+    x = 6 # ou 5, não lembro
+    while x < len(links):
         if y == opcao:
-            linkGPG = x
+            linkGPG = links[x]
         y += 1
-
-    
-
+        x +=1
     # Baixa o arquivo para realziar a verificação
     sucessoGPG = subprocess.run(["wget", "-o", "verificar.gpg", "-f", link])
     if sucessoGPG == 0:    
         print("Arquivo GPG baixado com sucesso")
     else:
         print("Houve um erro ao baixar o arquivo GPG")
-
     linkSHA = linkGPG.slipt(".gpg")
     sucessoSHA = subprocess.run(["wget", "-o", "sha256sum.txt", "-f", linkSHA])
     if sucessoSHA == 0:    
@@ -63,7 +58,6 @@ def verificarCheck(checksum):
 
 # Função principal que rodará o programa
 def main():
-
     try:
       opts, args = getopt.getopt(argv,"hc:",["check="])
     except getopt.GetoptError as err:
@@ -76,10 +70,11 @@ def main():
          sys.exit()
       elif opt in ("-c", "--check"):
          arquivo = arg
-
     # Descobre o link do checksum e baixa ele
     link = pesquisarBanco(arquivo)
     baixarCheck(link)
-
     # Verifica o checksum
     verificarCheck("sha256sum.txt")
+
+if __name__ == "__main__":
+    main()
